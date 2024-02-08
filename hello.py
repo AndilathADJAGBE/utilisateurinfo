dcad2b6fafa53c661f61fc8c756b4618056d8a33
Andilath ADJAGBE <adjandilath@gmail.com>
2024-02-08 17:04:39 +0100

    third commit

--- hello.py
+++ hello.py
@@ -3,3 +3,7 @@ prenom = input ("entrez votre prenom : ")
 print ("bonjour", prenom, nom)
 age = input ("entrez votre age : ")
 print ("bonjour", prenom, nom,"et vous avez" ,age)
+
+annee = 2024
+annee_naissance =annee - int(age)
+print ("votre année de naissance est", annee_naissance)
